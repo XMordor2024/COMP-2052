07d461c078eb6b8a279881599d0f7ff988a577c9
XMordor2024 <zequivid12@gmail.com>
2025-04-29 11:39:29 -0400

    update file

---
+++ hash/app.py
@@ -0,0 +1,27 @@
+from flask import Flask, session
+app = Flask(__name__)
+
+
+app.secret_key = "clave_secreta"
+
+
+@app.route("/login")
+def login():
+    session["usuario"] = "Juan"
+    return "Usuario autenticado"
+
+@app.route("/user")
+def get_user():
+    usuario = session.get("usuario")
+    if usuario:
+        return f"Usuario registrado: {usuario}"
+    else:
+        return "Usuario NO autenticado"
+
+@app.route("/logout")
+def logout():
+    session.pop("usuario", None)
+    return "Sesión cerrada"
+
+if __name__ == '__main__':
+    app.run(debug=True)
