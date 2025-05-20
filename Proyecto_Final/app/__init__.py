7816b23a5f63ccf0fba6e4640512d58c9944aeba
XMordor2024 <zequivid12@gmail.com>
2025-05-20 13:55:12 -0400

    update file

--- Proyecto_Final/app/__init__.py
+++ Proyecto_Final/app/__init__.py
@@ -17,10 +17,7 @@ def create_app():
     # Importar y registrar los blueprints
     from app.routes import main  # Rutas principales
     from app.auth_routes import auth  # Rutas de autenticación
-    from app.ticket_routes import tickets  # Rutas para gestión de tickets
-
     app.register_blueprint(main)
     app.register_blueprint(auth)
-    app.register_blueprint(tickets, url_prefix='/tickets')  # Prefijo para las rutas de tickets
 
     return app
